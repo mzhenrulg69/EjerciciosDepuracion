f93b31c34ae131117bbee8eeb4798b209d28b8b2
mzhenrulg69 <mzhenrulg69@educastur.org>
2024-11-20 17:10:25 +0100

    primer commit

---
+++ src/ejercicios/depuracion/Ejemplo2While.java
@@ -0,0 +1,25 @@
+package ejercicios.depuracion;
+
+/* 
+ * programa que lee un número n y muestra n asteriscos
+ */
+import java.util.*;
+public class Ejemplo2While {
+    public static int pedirNumero(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Introduce un numero: ");
+        return sc.nextInt();
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n, contador = 0;
+        System.out.print("Introduce un número: ");
+        n = sc.nextInt();
+        while (contador< n){
+               System.out.println(" * ");
+               System.out.println("contador:"+ contador );
+               contador++;
+        }
+    }
+}
+
